cd34bf16b018397ae98f6317254f48396e8b8601
pranit67 <pranit67@gmail.com>
2023-02-06 17:27:16 +0530

    Added: code for Welcome Message

---
+++ src/main/java/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling Simulator Program");
+    }
+}
