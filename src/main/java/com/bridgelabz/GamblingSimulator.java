f4d1106d6256863528677861884261a85dfbe3f5
pranit67 <pranit67@gmail.com>
2023-02-06 17:30:39 +0530

    Added: Gambler stake of every day and bet every game

--- src/main/java/com/bridgelabz/GamblingSimulator.java
+++ src/main/java/com/bridgelabz/GamblingSimulator.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator Program");
+        int stake = 100;
+        int bet = 1;
     }
 }
