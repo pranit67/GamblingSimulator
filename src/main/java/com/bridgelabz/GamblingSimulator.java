36f8974dd02316594d8baa146022fdb24bfe7203
pranit67 <pranit67@gmail.com>
2023-02-06 17:35:39 +0530

    Added: code for Gambler make a bet of so either win or lose

--- src/main/java/com/bridgelabz/GamblingSimulator.java
+++ src/main/java/com/bridgelabz/GamblingSimulator.java
@@ -5,5 +5,17 @@ public class GamblingSimulator {
         System.out.println("Welcome to Gambling Simulator Program");
         int stake = 100;
         int bet = 1;
+
+        double Betting = Math.floor(Math.random()*100)%2;
+        if (Betting ==bet) {
+            stake++;
+            System.out.println("Gambler is winning $1");
+            System.out.println("stake = " +stake);
+        }
+        else {
+            stake--;
+            System.out.println("Gambler is loosing");
+            System.out.println("stake = " +stake);
+        }
     }
 }
